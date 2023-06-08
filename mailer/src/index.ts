e0636cca9126c1d54e6bd5eda64375b18fc19e89
Saiki <GSaiki26@gmail.com>
2023-06-08 13:44:53 -0300

    Added: Server working

--- mailer/src/index.ts
+++ mailer/src/index.ts
@@ -1,2 +1,36 @@
+// Libs
+import cluster from "cluster";
+import { cpus } from "os";
+
+import ServerModel from "./models/serverModel";
+import MigrationsModel from "./models/migrationsModel";
+import LoggerModel from "./models/loggerModel";
+
+// Data
+const logger = LoggerModel.get("SERVER");
+
+// Functions
+async function main(): Promise<void> {
+  if (cluster.isPrimary) {
+    // Define the routes and run the migrations.
+    await MigrationsModel.migrations();
+    ServerModel.defineRoutes();
+
+    // Fork the cluster if MULTI_PROCESSING is enabled.
+    if (process.env.MULTI_CLUSTER! == "true") {
+      for (let i = 0; i < cpus().length; i++) {
+        cluster.fork();
+      }
+      return;
+    }
+  }
+
+  // Start the server.
+  ServerModel.start();
+}
+
+// Events
+cluster.on("exit", (worker, code) => logger.info(`Cluster exited with code: ${code}`));
+
 // Code
-console.info("Hello world!");
+main();
--- mailer/src/models/databaseModel.ts
+++ mailer/src/models/databaseModel.ts
@@ -1,13 +1,13 @@
 // Libs
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
 // Class
 class DatabaseModel {
   private static user = process.env.MONGO_INITDB_ROOT_USERNAME!;
   private static pass = process.env.MONGO_INITDB_ROOT_PASSWORD!;
 
-  public static connect(): Promise<Mongoose> {
-    return mongoose.connect("mailer-db", {
+  public static connect(): Connection {
+    return mongoose.createConnection("mongodb://mailer-db", {
       user: this.user,
       pass: this.pass,
       autoCreate: true
